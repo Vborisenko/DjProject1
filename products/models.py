5c62a9cc899ccf27fadf0f5e2ed446408a8e4635
bor <375445894910vitaly@gmail.com>
2020-01-14 23:32:07 +0200

    make app

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 # Remember to make a migration, after changing smth here
 class Product(models.Model):
-    title          = models.TextField()
-    description    = models.TextField()
-    price          = models.TextField()
-    summary        = models.TextField(default='This is cool')
+    title          = models.CharField(max_length=100)  #max_lenght is required
+    description    = models.TextField(blank=True, null=True)
+    price          = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary        = models.TextField()
