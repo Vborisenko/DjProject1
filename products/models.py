a8a1e9776276e2b65db6ec89f14f4592a78e3ec2
bor <375445894910vitaly@gmail.com>
2020-01-15 01:32:29 +0200

    fields in models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.db import models
 
 
@@ -6,5 +8,7 @@ from django.db import models
 class Product(models.Model):
     title          = models.CharField(max_length=100)  #max_lenght is required
     description    = models.TextField(blank=True, null=True)
-    price          = models.DecimalField(decimal_places=2, max_digits=10000)
+    price          = models.DecimalField(default=3000, decimal_places=2, max_digits=10000)
     summary        = models.TextField()
+    feature        = models.BooleanField(default=True)
+    # blank means that this field is required, null provides for database
