77c4c93df5952438a5e408440b229edf7ffed69d
bor <375445894910vitaly@gmail.com>
2020-01-19 17:37:32 +0200

    Widgets in html form

--- products/forms.py
+++ products/forms.py
@@ -14,6 +14,20 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title       = forms.CharField()
-    description  = forms.CharField()
-    price       = forms.DecimalField()
+    title       = forms.CharField(required=True, label='',
+                                  widget=forms.TextInput(
+                                      attrs={
+                                          "placeholder": "Your title"
+                                      }))
+    description  = forms.CharField(required=False,
+                                   label='',
+                                   widget=forms.Textarea(
+                                       attrs={
+                                           "class": "home-page",
+                                           "rows": 2,
+                                           "id": "desc_textarea",
+                                           "cols": 22,
+                                           "placeholder": "Your description"
+                                       }))
+    price       = forms.DecimalField(initial=199.99,
+                                     label='')
