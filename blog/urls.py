c019072fd9cb3037adeca9a66d895b8c8f0cd168
bor <375445894910vitaly@gmail.com>
2020-01-21 22:04:25 +0200

    Class Based Views - DetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import (ArticleView)
+from .views import (ArticleView,
+                    ArticleDetailView,)
 
 app_name = "articles"               # namespace
 urlpatterns = [
     path('', ArticleView.as_view(), name='article-list'),
-
+    path('<int:id>', ArticleDetailView.as_view(), name='article-detail'),   # <int:pk> is default. If you want to change
+    path('<int:id>', ArticleDetailView.as_view(), name='article-detail'),   # you should to override method in ViewsClass
+    # path('<int:id>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (CreateView,
                                   DetailView,
-                                  ListView)
+                                  ListView,)
 from .models import Article
 # Create your views here.
 
@@ -9,3 +9,12 @@ from .models import Article
 class ArticleView(ListView):
     template_name   = 'articles/article_list.html'
     queryset        = Article.objects.all()  # blog/<modelname>_list.html
+
+
+class ArticleDetailView(DetailView):
+    template_name   = 'articles/article_detail.html'
+    queryset        = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
